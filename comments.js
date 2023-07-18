eadc49b8b2107db2a0eab014a126a0f18ca2800b
Darshit Shah <darshit2468@gmail.com>
2023-07-18 09:35:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,45 +1,30 @@
-// create web server
-var express = require('express')
-var app = express()
-// create server
-var server = require('http').createServer(app)
-// create socket
-var io = require('socket.io')(server)
-// create mongoose
-var mongoose = require('mongoose')
-// connect to mongo db
-mongoose.connect('mongodb://localhost/comments')
-// create schema
-var Schema = mongoose.Schema
-// create model
-var Comment = mongoose.model('Comment', new Schema({
-  name: String,
-  comment: String
-}))
-// listen to port 3000
-server.listen(3000)
-// use public folder
-app.use(express.static(__dirname + '/public'))
-// get request
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html')
-})
-// create connection
-io.on('connection', function(socket){
-  console.log('a user connected')
-  // get comment
-  socket.on('get comment', function(){
-    Comment.find({}, function(err, docs){
-      if(err) throw err
-      socket.emit('get comment', docs)
-    })
-  })
-  // post comment
-  socket.on('post comment', function(data){
-    var comment = new Comment({name: data.name, comment: data.comment})
-    comment.save(function(err){
-      if(err) throw err
-      io.emit('post comment', data)
-    })
-  })
-})
+// Create web server
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  const { id } = req.params;
+  const comments = commentsByPostId[id] || [];
+  res.status(200).send(comments);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const { id } = req.params;
+  const { content } = req.body;
+  const commentId = randomBytes(4).toString('hex');
+  const comments = commentsByPostId[id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log('Listening on port 4001');
+});
